1782f9b4ba56d5ce3ec6a6a87d19a3ca7cb59672
Abdul Hadi Butt <hadibutt476@gmail.com>
2023-04-25 02:07:45 +0500

    Final commit

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    public function children()
+    {
+        return $this->hasMany(MenuItem::class, 'parent_id')->with('children');
+    }
 
+    public static function getMenuItems()
+    {
+        return static::whereNull('parent_id')->with('children')->get();
+    }
 }
